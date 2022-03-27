854b4b91f6c4730776dab9efcd4a74307929abc3
Mina Shenouda <mina.shenouda@vretta.com>
2022-03-26 17:22:44 -0700

    Typescript 1st commit

---
+++ dev2.ts
@@ -0,0 +1,333 @@
+import { time } from "console";
+import { allowedNodeEnvironmentFlags } from "process";
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+};
+
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    function dfs(root: TreeNode | null): void {
+        if(root == null)
+            return;    
+        values = [...values, root.val];
+        if (root.left != null)  
+            dfs(root.left);  
+        if (root.right != null)       
+            dfs(root.right);
+    }
+    let values: number[] = [];
+    dfs(root);
+    return values;    
+};
+
+// const result = preorderTraversal(root);
+// console.log(`Result is ${result}`)
+
+function maxAscendingSum(nums: number[]): number {
+    
+    let partSum: number = nums[0];
+    let maxSum: number = 0;
+    for(let i = 1; i < nums.length; i++)
+    {
+        if (nums[i] > nums[i-1])
+            partSum += nums[i];
+        else {
+            maxSum = Math.max(maxSum, partSum);
+            partSum = nums[i];
+        }
+    }
+    return Math.max(partSum, maxSum);
+};
+// const nums: number[] = [10,20,30,5,10,50];
+// const nums: number[] = [10,20,30,40,50];
+// const nums = [12,17,15,13,10,11,12]
+// const result = maxAscendingSum(nums);
+// console.log(`Result is ${result}`)
+function minimumDifference(nums: number[], k: number): number {
+
+    nums.sort((a, b) => b - a);
+    let partSum: number;
+    let minSum: number = nums.reduce((a, b)=> a + b,0);
+    let i: number = 0;
+    let j: number = 0;
+    do {
+        j = i + k -1;
+        if ( j < nums.length)
+        {   partSum = nums[i] - nums[j];
+            minSum = Math.min(partSum, minSum);
+        }
+        i++;
+    } while(j < nums.length);
+
+    return minSum;
+};
+
+// const nums: number[] = [9,4,1,7], k: number = 2;
+// const nums: number[] = [90], k = 1
+// const nums: number[] = [9,4], k: number = 2;
+// const result = minimumDifference(nums, k);
+// console.log(`Result is ${result}`)
+function numDifferentIntegers(word: string): number {
+    const re: RegExp =  (/[0-9]+/g);
+    let result: any[]  = word.match(re);
+    let i :number = 0;
+    while (result != null && i < result.length) {
+        while(result[i][0] == '0') {
+            result[i] = result[i].slice(1);
+        }
+        i++;
+    }; 
+    const s = [...new Set(result)]
+    return s.length;
+};
+// const word = "a123bc34d8ef34"
+// const word = "a1b01c001"
+//const word = "0a0"
+// const word = "035985750011523523129774573439111590559325";
+// const result = numDifferentIntegers(word);
+// console.log(`Result is ${result}`)
+
+function truncateSentence(s: string, k: number): string {
+    const result = s.split(" ");
+    result.length = k;
+    return result.join(" ");
+};
+// const s = "Hello how are you Contestant", k = 4;
+// const result = truncateSentence(s, k);
+// console.log(`Result is ${result}`)
+function areOccurrencesEqual(s: string): boolean {
+    let counter: any = {};
+    for(let chr of s) {
+        if (!counter[chr])
+           counter[chr] = 1;
+        else
+            counter[chr]++;
+    }
+    const values = Object.values(counter);
+    return [...new Set(values)].length == 1;
+};
+// const s = "abacbc"
+// const result = areOccurrencesEqual(s);
+// console.log(`Result is ${result}`)
+
+function shift(s: string, shift: number) {
+    const code = s.charCodeAt(0) + shift;
+    return String.fromCharCode(code);
+}
+
+function replaceDigits(s: string): string {
+    let result: string[] = s.split("");
+
+    for(let i = 0 ; i < s.length; i++)
+    {   if (i % 2 == 1)
+            result[i] = shift(s[i-1], Number(s[i]));
+    }
+    return result.join("");
+};
+// const s = "a1c1e1";
+// const result = replaceDigits(s);
+// console.log(`Result is ${result}`)
+function minOperations(nums: number[]): number {
+    if(nums.length == 1)
+        return 0;
+
+    let counter: number = 0;
+    for(let i = 1; i < nums.length; i++)
+    {
+        const expected: number = Math.max(nums[i-1]+1, nums[i])
+        counter += expected - nums[i];
+        nums[i] = expected;    
+    }
+    return counter;
+};
+// const nums = [1,1,1];
+// const nums: number[] = [1,5,2,4,1]
+// const result = minOperations(nums);
+// console.log(`Result is ${result}`)
+function getMinDistance(nums: number[], target: number, start: number): number {
+    if (nums.length == 1)
+        return 0;
+
+    const indices = nums.map((value, index) => {
+        if (value == target)
+            return index;
+    }).filter(x => x != undefined);
+    
+    let minDis: number = nums.length;
+    for(let index of indices) {
+        minDis = Math.min(Math.abs(index-start), minDis);
+    }
+    
+    return minDis;
+};
+// const nums = [1,2,3,4,5], target = 5, start = 3;
+// const nums = [1,1,1,1,1,1,1,1,1,1], target = 1, start = 0;
+// const result = getMinDistance(nums, target, start);
+// console.log(`Result is ${result}`);
+
+function maximumPopulation(logs: number[][]): number {
+    const years: number[] = [];
+    let partMax: number = 0;    
+    let finalMax: number = 0;
+    let result: number[] = [];
+    for(const log of logs) {
+        partMax = Math.max(log[1] - log[0] - 1, partMax);
+        if (partMax > finalMax) 
+        {   finalMax = partMax;
+            result = [];
+            result.push(log[0]) 
+        } else {
+            result.push(log[0]) 
+        }
+    }
+    console.log(result);
+    result.sort((a, b)=> a-b);
+    return result[0];
+};
+// const logs: number[][] = [[1993,1999],[2000,2010]];
+// const result = maximumPopulation(logs);
+// console.log(`Result is ${result}`);
+function inorderTraversal(root: TreeNode | null): number[] {
+    function dfs(root: TreeNode | null): void {
+        if(root == null)
+            return;
+        if(root.left != null)
+            dfs(root.left);
+        result = [...result, root.val];
+        if(root.right != null)
+            dfs(root.right);     
+    };
+    let result: number[] = [];
+    dfs(root);
+    return result;
+};
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    function dfs(root: TreeNode | null): void {
+        if(root == null)
+            return;
+        if(root.left != null)
+            dfs(root.left);
+        if(root.right != null)
+            dfs(root.right);     
+        result = [...result, root.val];
+    };
+    let result: number[] = [];
+    dfs(root);
+    return result;
+};
+
+function sumOfLeftLeaves(root: TreeNode | null): number {
+    function dfs(root: TreeNode | null): void {
+        if (root == null)
+            return;
+        if(root.left != null) {
+            dfs(root.left);
+            if (root.left == null)
+                result = [...result, root.val];
+        }
+        if(root.right != null)
+            dfs(root.right);  
+    }
+    let result: number[] = [];
+    dfs(root);
+    const val: number = result.reduce((a, b)=> a +b, 0);
+    return val;
+}
+const root = new TreeNode(3);
+const nine = new TreeNode(9);
+const twenty = new TreeNode(20);
+
+const fivteen = new TreeNode(15);
+const seven = new TreeNode(7);
+twenty.right = seven;
+twenty.left = fivteen;
+root.left = nine;
+root.right = twenty;
+
+// const result: number = sumOfLeftLeaves(root);
+// console.log(`Result is ${result}`);
+
+function largestOddNumber(num: string): string {
+    if (Number(num) %2 ==1)
+        return num;
+    let j:number =0;
+    let result = "";
+    for(let i:number =0; i < num.length; i++)
+    {   
+        if (Number(num[i]) %2 == 1) {
+            for(let k: number=j; k <= i; k++)
+                result = result.concat(num[k]);
+            j = i + 1;
+        }
+    }
+    return result;
+};
+//const  num = "52";
+// const  num = "10133890"; 
+// const result: string = largestOddNumber(num);
+// console.log(`Result is ${result}`);
+function canBeTypedWords(text: string, brokenLetters: string): number {
+    const words = text.split(" ");
+    let failed: number = 0;
+    for(const word of words) {
+        for(const letter of brokenLetters.split(""))
+            if (word.includes(letter))
+            {
+                failed++;
+                break;
+            }
+    }
+    return words.length - failed;
+};
+//const text = "hello world", brokenLetters = "ad";
+// const text = "leet code", brokenLetters = "lt"
+// const result: number = canBeTypedWords(text, brokenLetters);
+// console.log(`Result is ${result}`);
+function sortSentence(s: string): string {
+    let words = s.split(" ");
+    function compare(a: string, b: string) {
+        if (a[a.length-1] < b[b.length-1])
+            return -1;
+        if (a[a.length-1] > b[b.length-1])
+            return 1;
+    }
+    words.sort((a, b) => compare(a, b))
+    words = words.map(x => x.slice(0,x.length-1));
+    return words.join(" ");
+};
+// const s: string = "is2 sentence4 This1 a3";
+// const result: string = sortSentence(s);
+// console.log(`Result is ${result}`);
+function reformatNumber(number: string): string {
+    const nums = number.replace(/[\-\ \.]/g, "");
+    let i: number = 0;
+    let result: string[] = [];
+    while( i< nums.length) {
+        if (nums.length % 4 == 0)
+        {   result.push(nums.slice(0, 4));
+            i += 4;
+        } else if ( nums.length % 2 == 0) {
+            result.push(nums.slice(0, 2));
+            i += 2
+        } else {
+            result.push(nums.slice(0, 3));
+            i += 3;
+        }
+    }
+    console.log(result);
+    console.log(nums);
+    return "";
+};
+// const number = "1-23-45 6";
+// const result: string = reformatNumber(number);
+// console.log(`Result is ${result}`);
+export {};
---
+++ dp.ts
@@ -0,0 +1,222 @@
+import { time } from "console";
+import { allowedNodeEnvironmentFlags } from "process";
+
+function maxArea(height: number[]): number {
+    return 0;
+};
+
+function hIndex(citations: number[]): number {
+    
+    if (citations.length <= 1 && citations[0] == 0)
+        return 0;
+    if (citations.length <= 1 && citations[0] != 0)
+        return 1;
+
+    citations.sort((a, b)=> a-b);
+    let result: number = 0;
+    let n: number = citations.length;
+    for(let i in citations) {
+        const bigger: number = n - Number(i) + 1;
+        const smaller: number = Number(i) + 1;
+        console.log(bigger, smaller);
+        if (bigger == smaller) {
+            result = Math.max(result, citations[i]);
+        }
+    }
+    return result;
+};
+
+
+// function searchRange(nums: number[], target: number): number[] {
+
+//     let result: number[] = [];
+//     let left: number = 0;
+//     let right: number = nums.length-1;
+//     while(result.length < 2) {
+//         let mid: number = Math.floor((left + right)/ 2);
+//         if (nums[mid] == target) {
+//             result.push(mid);
+//         }
+//         else if ( nums[mid] < target) 
+//             left = mid + 1;
+//         else if ( nums[mid] > target)
+//             right = mid - 1;
+//     };
+//     return result;
+// };
+
+// const height = [1,8,6,2,5,4,8,3,7]
+// const result: number = maxArea(height);
+// console.log(`Result is ${result}`);
+
+//const citations = [3,0,6,1,5];
+// const citations = [1,3,1];
+// const citations = [100]
+//const citations = [0, 1];
+// const result: number = hIndex(citations);
+// console.log(`Result is ${result}`);
+
+// const nums = [5,7,7,8,8,10], target = 8
+// const result: number[] = searchRange(nums, target);
+// console.log(`Result is ${result}`);
+
+function canPartition(nums: number[]): boolean {
+    if (nums.length < 2)
+        return false;
+    let sum: number = nums.reduce((a, b)=> a+b, 0);
+    if (sum % 2 == 1)
+        return false;
+    sum = sum / 2;
+    const rows = nums.length + 1;
+    const columns = sum + 1;
+    console.log(rows, columns);
+    let dp: boolean[][] = [...Array(rows)].map(row => [...Array(columns)].map(x => false));
+    
+    dp[0][0] = true; 
+    for(let i=1; i< rows; i++)
+        dp[i][0] = false;
+    
+    for(let i=1; i<columns; i++) 
+        dp[0][i] = false;
+   
+    for(let i=1; i<rows; i++) {
+        for(let j=1; j<columns; j++)
+        {
+            if (j < nums[i-1])
+                dp[i][j] = dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]    
+        }
+    }
+    console.log(...dp);
+    return dp[rows-1][columns-1];
+};
+
+// const nums: number[] = [1,5,11,5];
+// const result: boolean = canPartition(nums);
+// console.log(`Result is ${result}`);
+
+function findTargetSumWays(nums: number[], target: number): number {
+    const rows: number = nums.length + 1;
+    const sum: number = nums.reduce((a, b) => a +b, 0);
+    if (sum < target)
+        return 0;
+
+    const columns: number = Math.floor((sum + target) / 2) + 1;
+    let dp: number[][] = [...Array(rows)].map(row => [...Array(columns)].map(x => 0));
+    
+    //1st column is always 1 because there's 1 way to form an empty set, regardless of items found. 
+    for(let i=0; i<rows; i++)
+        dp[i][0] = 1
+ 
+    for(let i=1; i<rows; i++) {
+        for(let j=1; j<columns; j++) {
+            if (j < nums[i-1])
+                dp[i][j] = dp[i-1][j]
+            else
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]] 
+        }
+    }
+    console.log(...dp);
+    return dp[rows-1][columns-1];
+};
+
+// const nums: number[] = [1,1,1,1,1], target = 3;
+// const nums: number[] = [0,0,0,0,0,0,0,0,1], target = 1;
+// const result: number = findTargetSumWays(nums, target);
+// console.log(`Result is ${result}`);
+
+function maxSumDivThree(nums: number[]): number {
+    const rows: number = nums.length +1;
+    const columns: number = nums.reduce((a, b)=> a +b, 0) + 1;
+    let dp: number[][] = [...Array(rows)].map(row => [...Array(columns).map(x => 0)]);
+    for(let i=0; i< columns; i++)
+        dp[0][i] = 0;
+    for(let i=0; i<rows; i++)
+        dp[i][0] = 1;   
+
+    // for(let i = 1; i< rows; i++)
+    //    for(let j = 1; j < columns; j++)
+    //         if ( j < nums[i-1]) 
+    //             dp[i][j] = dp[i-1][j];
+    //         else if ( j % 3 == 0)
+    //             dp[i][j] = dp[i-1] + dp[i-1][j-nums[i-1]];
+    //         else
+    //             dp[i][j] = dp[i][j-1];
+    console.log(...dp);
+    return dp[rows-1][columns-1];
+};
+// const nums = [3,6,5,1,8];
+// const nums =[1,2,3,4,4];
+// const result: number = maxSumDivThree(nums);
+// console.log(`Result is ${result}`);
+
+function largestDivisibleSubset(nums: number[]): number[] {
+    nums.sort((a, b) => a - b);
+	
+	// each index will store the subset with which the num at that index satisfies the property
+	// initially adding the num itself as the subset
+    const dp = new Array(nums.length);
+    nums.forEach((num, idx) => dp[idx] = [num]);
+    
+    let maxLenSet = [];
+    for(let i = 0; i < nums.length; ++i) {
+        for(let j = 0; j < i; ++j) {
+			// for each num[j] <= num[i], check if it satisfies the property and if adding num[i] to that subset is greater than the curr subset
+            if((nums[i] % nums[j] === 0 || nums[j] % nums[i] === 0) && dp[i].length < dp[j].length + 1) {
+                dp[i] = [...dp[j]].concat(nums[i]);
+            }
+        }
+        if(maxLenSet.length < dp[i].length) {
+            maxLenSet = dp[i];
+        }
+    }
+    console.log(...dp);
+    return maxLenSet;
+};
+// const nums = [1,2,3];
+// const nums = [1,2,4,8,16];
+//const nums= [3,17];
+// const nums = [4,8,10,240];
+// const result: number[] = largestDivisibleSubset(nums);
+// console.log(`Result is ${result}`);
+
+
+function convertStr2Num(nums: string) {
+    const num: number =  Number(nums);
+    // console.log("Num", num)
+    return num;
+}
+
+
+function restoreIpAddresses(s: string): string[] {
+    const arr: string[] = s.split("");
+    console.log("Array", arr);
+    let tmp: string = "";
+    let result: string = "";
+    function dfs(arr: string[], count:number) {
+        for(let i =0; i<arr.length; i++)
+        {   
+            console.log(...arr, count);
+            tmp += arr[i];
+            const val = convertStr2Num(tmp);
+            if (count <= 4 &&  val>= 0 && val<= 255) {                    
+                arr.splice(i, 1); 
+                dfs(arr, count+1);
+                tmp = tmp.slice(0, -1);
+                console.log("Tmp",  tmp);
+                result += tmp + ".";
+                tmp = "";
+            } else
+                break;
+        }
+    }
+    dfs(arr, 0);
+    console.log(result);
+    return arr;
+};
+const s = "101023";
+const result: string[] = restoreIpAddresses(s);
+console.log(`Result is ${result}`);
+
+
